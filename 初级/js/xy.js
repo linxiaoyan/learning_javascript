1ea02d7b129fad86a28a2d6cae101f1375f2b6c1
linxiaoyan <linxiaoyan87@foxmail.com>
2016-02-16 16:01:39 +0800

    封装几个函数

--- 初级/js/xy.js
+++ 初级/js/xy.js
@@ -84,25 +84,116 @@ function opacity(obj, num, target, frequency, endFn) {
   }, frequency);
 }
 
-function getDate(obj) {
-  setInterval(showDate, 1000);
-  showDate();
-
-  function showDate() {
-    var oDate = new Date();
-    var iYear = oDate.getFullYear();
-    var iMon = oDate.getMonth() + 1;
-    var iDay = oDate.getDay();
-    var iWeek = iDay - 1;
-    var iHours = oDate.getHours();
-    var iMin = oDate.getMinutes();
-    var iSec = oDate.getSeconds();
-    var week = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'];
-
-    obj.innerHTML = iYear + '年' + iMon + '月' + iDay + '日' + ' ' + week[iWeek] + ' ' + toTwo(iHours) + ':' + toTwo(iMin) + ':' + toTwo(iSec);
-  }
-}
-
 function toTwo(num) {
   return num >= 10 ? ('' + num) : ('0' + num);
 }
+
+function getDate() {
+  var oDate = new Date();
+  var iYear = oDate.getFullYear();
+  var iMon = oDate.getMonth() + 1;
+  var iDay = oDate.getDay();
+  var iWeek = iDay - 1;
+  var iHours = oDate.getHours();
+  var iMin = oDate.getMinutes();
+  var iSec = oDate.getSeconds();
+  var week = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'];
+  var str = iYear + '年' + iMon + '月' + iDay + '日' + ' ' + week[iWeek] + ' ' +
+            toTwo(iHours) + ':' + toTwo(iMin) + ':' + toTwo(iSec);
+  return str;
+}
+
+function getPos(obj) {
+  var pos = {top:0, left:0};
+
+  while (obj) {
+    pos.top += obj.offsetTop;
+    pos.left += obj.offsetLeft;
+    obj = obj.offsetParent;
+  }
+
+  return pos;
+}
+
+function getElementsByClassName(obj, tagName, className) {
+  var aClassName = className.split(', ');
+  var elements = obj.getElementsByTagName(tagName);
+  var resElements = [];
+
+  for (var i = 0; i < aClassName.length; i++) {
+    for (var j = 0; j < elements.length; j++) {
+      var arrClassName = elements[j].className.split(' ');
+
+      if (-1 != arrIndexOf(arrClassName, aClassName[i])) {
+        resElements.push(elements[j]);
+      }
+    }
+  }
+
+  return resElements;
+}
+
+function addClass(obj, className) {
+  if (obj.className == '') {
+    obj.className = className;
+  } else {
+    var aClassName = obj.className.split(' ');
+
+    if (-1 == arrIndexOf(aClassName, className)) {
+      obj.className += ' ' + className;
+    }
+  }
+}
+
+function arrIndexOf(arr, val) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] == val) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function removeClass(obj, className) {
+  if (obj.className != '') {
+    var aClassName = obj.className.split(' ');
+    var n = arrIndexOf(aClassName, className);
+
+    if (-1 != n) {
+      aClassName.splice(n, 1);
+      obj.className = aClassName.join(' ');
+    }
+  }
+}
+
+function getChecked(form, name) {
+  var arr = [];
+
+  for (var i = 0; i < form[name].length; i++) {
+    if (form[name][i].checked) {
+      arr.push(form[name][i].value);
+    }
+  }
+
+  if (form[name][0].type == 'radio') {
+    return arr[0];
+  } else if (form[name][0].type == 'checkbox') {
+    return arr;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
